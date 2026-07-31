75487ed56365b8ccfc699488e221f917839ce61a
Sumit Rathor <sumitrathor142272@gmail.com>
2026-07-31 09:05:05 +0530

    Add change

---
+++ Server/fpv_car/cam/upload.php
@@ -0,0 +1,15 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
+
+$baseDir = __DIR__;
+$data = file_get_contents("php://input");
+
+file_put_contents("$baseDir/latest.jpg", $data);
+echo "OK";
---
+++ Server/fpv_car/get.php
@@ -0,0 +1,34 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Pragma: no-cache");
+header("Expires: 0");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
+
+$file = __DIR__ . "/state.txt";
+
+$default = [
+    "cmd" => "S",
+    "mode" => "0",
+    "cam" => "1",
+    "fs" => "255",
+    "bs" => "255"
+];
+
+if (file_exists($file)) {
+    $raw = file_get_contents($file);
+    $decoded = json_decode($raw, true);
+    if (is_array($decoded)) {
+        echo json_encode(array_merge($default, $decoded));
+    } else {
+        echo json_encode($default);
+    }
+} else {
+    echo json_encode($default);
+}
